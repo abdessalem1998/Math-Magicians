437d1e4ec3cbe1c8fc70291f80e1fe4639133eb3
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-14 10:41:38 +0100

    Refactoring using functional component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,60 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+const Calculator = () => {
+  const [values, setValues] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    // This binding is necessary to make `this` work in the callback
-    this.clickHandler = this.clickHandler.bind(this);
-  }
-
-  clickHandler(e) {
+  const clickHandler = (e) => {
     const pressedButton = e.target.name;
-    const updated = calculate(this.state, pressedButton);
-    this.setState(updated);
-  }
+    const updated = calculate(values, pressedButton);
+    setValues(updated);
+  };
 
-  render() {
-    const {
-      total,
-      next,
-      operation,
-    } = this.state;
-    return (
-      <div className="grid-container">
-        <div className="result">
-          {!total && !next && !operation
-            ? '0'
-            : `${total || ''} ${operation || ''} ${next || ''}`}
-        </div>
-        <button name="AC" onClick={this.clickHandler} type="button" className="keyboard nbr">AC</button>
-        <button name="+/-" onClick={this.clickHandler} type="button" className="keyboard nbr">+/-</button>
-        <button name="%" onClick={this.clickHandler} type="button" className="keyboard nbr">%</button>
-        <button name="÷" onClick={this.clickHandler} type="button" className="keyboard op">÷</button>
-        <button name="7" onClick={this.clickHandler} type="button" className="keyboard nbr">7</button>
-        <button name="8" onClick={this.clickHandler} type="button" className="keyboard nbr">8</button>
-        <button name="9" onClick={this.clickHandler} type="button" className="keyboard nbr">9</button>
-        <button name="x" onClick={this.clickHandler} type="button" className="keyboard op">×</button>
-        <button name="4" onClick={this.clickHandler} type="button" className="keyboard nbr">4</button>
-        <button name="5" onClick={this.clickHandler} type="button" className="keyboard nbr">5</button>
-        <button name="6" onClick={this.clickHandler} type="button" className="keyboard nbr">6</button>
-        <button name="-" onClick={this.clickHandler} type="button" className="keyboard op">-</button>
-        <button name="1" onClick={this.clickHandler} type="button" className="keyboard nbr">1</button>
-        <button name="2" onClick={this.clickHandler} type="button" className="keyboard nbr">2</button>
-        <button name="3" onClick={this.clickHandler} type="button" className="keyboard nbr">3</button>
-        <button name="+" onClick={this.clickHandler} type="button" className="keyboard op">+</button>
-        <button name="0" onClick={this.clickHandler} type="button" className="keyboard item0 nbr">0</button>
-        <button name="." onClick={this.clickHandler} type="button" className="keyboard nbr">.</button>
-        <button name="=" onClick={this.clickHandler} type="button" className="keyboard op">=</button>
+  const {
+    total,
+    next,
+    operation,
+  } = values;
+  return (
+    <div className="grid-container">
+      <div className="result">
+        {!total && !next && !operation
+          ? '0'
+          : `${total || ''} ${operation || ''} ${next || ''}`}
       </div>
-    );
-  }
-}
+      <button name="AC" onClick={clickHandler} type="button" className="keyboard nbr">AC</button>
+      <button name="+/-" onClick={clickHandler} type="button" className="keyboard nbr">+/-</button>
+      <button name="%" onClick={clickHandler} type="button" className="keyboard nbr">%</button>
+      <button name="÷" onClick={clickHandler} type="button" className="keyboard op">÷</button>
+      <button name="7" onClick={clickHandler} type="button" className="keyboard nbr">7</button>
+      <button name="8" onClick={clickHandler} type="button" className="keyboard nbr">8</button>
+      <button name="9" onClick={clickHandler} type="button" className="keyboard nbr">9</button>
+      <button name="x" onClick={clickHandler} type="button" className="keyboard op">×</button>
+      <button name="4" onClick={clickHandler} type="button" className="keyboard nbr">4</button>
+      <button name="5" onClick={clickHandler} type="button" className="keyboard nbr">5</button>
+      <button name="6" onClick={clickHandler} type="button" className="keyboard nbr">6</button>
+      <button name="-" onClick={clickHandler} type="button" className="keyboard op">-</button>
+      <button name="1" onClick={clickHandler} type="button" className="keyboard nbr">1</button>
+      <button name="2" onClick={clickHandler} type="button" className="keyboard nbr">2</button>
+      <button name="3" onClick={clickHandler} type="button" className="keyboard nbr">3</button>
+      <button name="+" onClick={clickHandler} type="button" className="keyboard op">+</button>
+      <button name="0" onClick={clickHandler} type="button" className="keyboard item0 nbr">0</button>
+      <button name="." onClick={clickHandler} type="button" className="keyboard nbr">.</button>
+      <button name="=" onClick={clickHandler} type="button" className="keyboard op">=</button>
+    </div>
+  );
+};
 export default Calculator;
