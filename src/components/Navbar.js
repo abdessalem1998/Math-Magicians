da88317e7d1a32037401257fb2c7df2b9a3fc0ee
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-19 00:01:12 +0100

    Style Navbar component with inline style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@ const Navbar = () => {
   const links = [
     {
       id: 1,
-      path: '/',
-      text: 'Home',
+      path: '/Quote',
+      text: 'Quote',
     },
     {
       id: 2,
@@ -12,17 +12,44 @@ const Navbar = () => {
     },
     {
       id: 3,
-      path: '/Quote',
-      text: 'Quote',
+      path: '/',
+      text: 'Home',
     },
   ];
 
   return (
     <nav>
-      <ul>
+      <ul style={{
+        listStyleType: 'None',
+        margin: '0',
+        padding: '0',
+        backgroundColor: '#333',
+        overflow: 'hidden',
+      }}
+      >
+        <li style={{
+          float: 'left',
+          color: 'white',
+          textAlign: 'center',
+          paddingLeft: '25px',
+          textDecoration: 'none',
+          fontSize: '30px',
+        }}
+        >
+          Math Magicians
+        </li>
         {
           links.map((link) => (
-            <li key={link.id}>
+            <li
+              key={link.id}
+              style={{
+                float: 'right',
+                color: 'white',
+                textAlign: 'center',
+                padding: '14px 16px',
+                textDecoration: 'none',
+              }}
+            >
               {link.text}
             </li>
           ))
