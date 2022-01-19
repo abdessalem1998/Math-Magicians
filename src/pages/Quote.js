39a07567e93ebbb97ebf2957c11611e25d59f7f9
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-19 15:57:56 +0100

    create Home and Quote component and style the calculator page

---
+++ src/pages/Quote.js
@@ -0,0 +1,9 @@
+const Quote = () => {
+
+  return (
+    <div>
+      <p>Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. -William Paul Thurston</p>
+    </div>
+  )
+}
+export default Quote;
